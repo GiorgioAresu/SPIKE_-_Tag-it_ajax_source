41043a98bf2b187c426f0a1b8f5b9a75c7a436ce
GiorgioAresu <giorgioaresu@gmail.com>
2015-09-11 11:32:01 +0200

    Use ajax as autocompletion source

--- Tag-it/Controllers/DefaultController.cs
+++ Tag-it/Controllers/DefaultController.cs
@@ -8,10 +8,17 @@ namespace Tag_it.Controllers
 {
     public class DefaultController : Controller
     {
+        List<string> list = new List<string> { "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby", "python", "c", "scala", "groovy", "haskell", "perl", "erlang", "apl", "cobol", "go", "lua" };
+
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetLabels(string search)
+        {
+            return Json(list.Where(x => x.StartsWith(search)));
         }
     }
 }
